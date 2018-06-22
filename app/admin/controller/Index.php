50c1df778354e573e68411ffb4ece4d3ea13bf02
shangjinghui <shang342530@126.com>
2018-06-22 17:09:51 +0800

    短信接口实现

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -8,6 +8,7 @@ use think\Request;
 use think\Url;
 use think\Session;
 use think\Config;
+use app\common\shortmessage\shortMessage;
 
 /**
 * 登录
@@ -19,10 +20,65 @@ class Index extends Admin
 	/**
 	 * 后台登录首页
 	 */
+
+
 	public function index()
 	{
 		$data['ip'] = Loader::model('LogRecord')->UniqueIpCount();
+		/*单发短信接口参数*/
+        /*$content = Array(
+            "KeyId"           =>'your access key id',//LTAIIFChBoFoWvQc
+            "KeySecret"       =>'your access key secret',//KbdeP1TliiTbDJ8z75c9yHqDNeWTKh
+            "phone"           =>'phone number',//18612410728
+            "signname"        =>'短信签名',//shangjinghui
+            "templatecode"    =>'短信模板ID',//SMS_137658496
+            "templateParam"   =>Array(
 
+            ),//可选，用于替换模板参数
+            "OutId"           =>'12345', //可选，设置发送短信流水号
+            "SmsUpExtendCode" =>'1234567'//可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+        );*/
+
+        /*群发短信接口参数*/
+        /*$content = Array(
+            "KeyId"           =>'your access key id',//LTAIIFChBoFoWvQc
+            "KeySecret"       =>'your access key secret',//KbdeP1TliiTbDJ8z75c9yHqDNeWTKh
+            "PhoneNumberJson" =>Array(
+                "1500000000",
+                "1500000001",
+            ),//群发号码
+            "SignNameJson"     =>Array(
+                "云通信",
+                "云通信2",
+            ),//支持不同号码不同短信签名
+            "templatecode"    =>'短信模板ID',//SMS_137658496
+            "TemplateParamJson"   =>Array(
+                array(
+                    "name" => "Tom",
+                    "code" => "123",
+                ),
+                array(
+                    "name" => "Jack",
+                    "code" => "456",
+                ),
+            )//可选，用于替换模板参数
+        );*/
+
+        /*测试*/
+        $content = Array(
+            "KeyId"           =>'LTAIIFChBoFoWvQc',//LTAIIFChBoFoWvQc
+            "KeySecret"       =>'KbdeP1TliiTbDJ8z75c9yHqDNeWTKh',//KbdeP1TliiTbDJ8z75c9yHqDNeWTKh
+            "phone"           =>'18612410728',//18612410728
+            "signname"        =>'shangjinghui',//shangjinghui
+            "templatecode"    =>'SMS_137658496',//SMS_137658496
+            "templateParam"   =>Array(
+                'name' =>'商景辉'
+            ),//可选，用于替换模板参数
+            "OutId"           =>'12345', //可选，设置发送短信流水号
+            "SmsUpExtendCode" =>'1234567'//可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+        );
+		/*$ob = new shortMessage();
+		$ob->sendSms($content);*/
 		$this->assign('data', $data);
 		return view();
 	}
---
+++ app/common/shortmessage/shortMessage.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 商景辉
+ * Date: 2018/6/22
+ * Time: 16:13
+ */
+namespace app\common\shortmessage;
+require_once "SignatureHelper.php";
+use app\common\shortmessage\SignatureHelper;
+
+
+class shortMessage
+{
+    /**
+     * 发送短信
+     */
+    function sendSms($con) {
+        $params = array ();
+        // *** 需用户填写部分 ***
+
+        // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
+        $accessKeyId = $con['KeyId'];
+        $accessKeySecret = $con['KeySecret'];
+
+        // fixme 必填: 短信接收号码
+        $params["PhoneNumbers"] = $con['phone'];
+
+        // fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+        $params["SignName"] = $con['signname'];
+
+        // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+        $params["TemplateCode"] = $con['templatecode'];
+
+        // fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
+        $params['TemplateParam'] = $con['templateParam'];
+
+        // fixme 可选: 设置发送短信流水号
+        $params['OutId'] = $con['OutId'];
+
+        // fixme 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+        $params['SmsUpExtendCode'] = $con['SmsUpExtendCode'];
+
+
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+        if(!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+        }
+
+        // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+        $helper = new SignatureHelper();
+
+        // 此处可能会抛出异常，注意catch
+        $content = $helper->request(
+            $accessKeyId,
+            $accessKeySecret,
+            "dysmsapi.aliyuncs.com",
+            array_merge($params, array(
+                "RegionId" => "cn-hangzhou",
+                "Action" => "SendSms",
+                "Version" => "2017-05-25",
+            ))
+        // fixme 选填: 启用https
+        // ,true
+        );
+
+        return $content;
+    }
+
+    /**
+     * 批量发送短信
+     */
+    function sendBatchSms($con) {
+        $params = array ();
+
+        // *** 需用户填写部分 ***
+
+        // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
+        $accessKeyId = $con['KeyId'];
+        $accessKeySecret = $con['KeySecret'];
+
+        // fixme 必填: 待发送手机号。支持JSON格式的批量调用，批量上限为100个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
+        $params["PhoneNumberJson"] = $con['PhoneNumberJson'];
+
+        // fixme 必填: 短信签名，支持不同的号码发送不同的短信签名，每个签名都应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+        $params["SignNameJson"] = $con['SignNameJson'];
+
+        // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+        $params["TemplateCode"] = $con['templatecode'];
+
+        // fixme 必填: 模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        // 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
+        $params["TemplateParamJson"] = $con['TemplateParamJson'];
+
+        // todo 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+        // $params["SmsUpExtendCodeJson"] = json_encode(array("90997","90998"));
+
+
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+        $params["TemplateParamJson"]  = json_encode($params["TemplateParamJson"], JSON_UNESCAPED_UNICODE);
+        $params["SignNameJson"] = json_encode($params["SignNameJson"], JSON_UNESCAPED_UNICODE);
+        $params["PhoneNumberJson"] = json_encode($params["PhoneNumberJson"], JSON_UNESCAPED_UNICODE);
+
+        if(!empty($params["SmsUpExtendCodeJson"] && is_array($params["SmsUpExtendCodeJson"]))) {
+            $params["SmsUpExtendCodeJson"] = json_encode($params["SmsUpExtendCodeJson"], JSON_UNESCAPED_UNICODE);
+        }
+
+        // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+        $helper = new SignatureHelper();
+
+        // 此处可能会抛出异常，注意catch
+        $content = $helper->request(
+            $accessKeyId,
+            $accessKeySecret,
+            "dysmsapi.aliyuncs.com",
+            array_merge($params, array(
+                "RegionId" => "cn-hangzhou",
+                "Action" => "SendBatchSms",
+                "Version" => "2017-05-25",
+            ))
+        // fixme 选填: 启用https
+        // ,true
+        );
+
+        return $content;
+    }
+
+    /**
+     * 短信发送记录查询
+     */
+    function querySendDetails() {
+
+        $params = array ();
+
+        // *** 需用户填写部分 ***
+
+        // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
+        $accessKeyId = "your access key id";
+        $accessKeySecret = "your access key secret";
+
+        // fixme 必填: 短信接收号码
+        $params["PhoneNumber"] = "17000000000";
+
+        // fixme 必填: 短信发送日期，格式Ymd，支持近30天记录查询
+        $params["SendDate"] = "20170710";
+
+        // fixme 必填: 分页大小
+        $params["PageSize"] = 10;
+
+        // fixme 必填: 当前页码
+        $params["CurrentPage"] = 1;
+
+        // fixme 可选: 设置发送短信流水号
+        $params["BizId"] = "yourBizId";
+
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+
+        // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+        $helper = new SignatureHelper();
+
+        // 此处可能会抛出异常，注意catch
+        $content = $helper->request(
+            $accessKeyId,
+            $accessKeySecret,
+            "dysmsapi.aliyuncs.com",
+            array_merge($params, array(
+                "RegionId" => "cn-hangzhou",
+                "Action" => "QuerySendDetails",
+                "Version" => "2017-05-25",
+            ))
+        // fixme 选填: 启用https
+        // ,true
+        );
+
+        return $content;
+    }
+}
